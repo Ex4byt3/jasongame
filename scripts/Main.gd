3f46f3ee688e27cff261e4d6e1326c4ef3a8efaf
ex4byt3 <github.kelp104@passmail.net>
2025-01-04 18:03:03 -0500

    add generators

---
+++ scripts/Main.gd
@@ -0,0 +1,27 @@
+extends Control
+
+var pointsLabel : Label
+var mainButton : Button
+var mult : int = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_get_nodes()
+	_connect_signals()
+
+func _get_nodes() -> void :
+	pointsLabel = get_node("PointLabel")
+	mainButton = get_node("MainButton")
+
+func _connect_signals() -> void :
+	mainButton.pressed.connect(_mainButton_pressed)
+
+func _mainButton_pressed() -> void :
+	Global.points += 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	_update_points()
+
+func _update_points() -> void :
+	pointsLabel.text = "Points : %s" %Global.points
